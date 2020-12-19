911e24e364d885546d6a3658fa54dc34f636b995
waiz khan <Waizk445@gmail.com>
2020-12-19 12:05:57 +0700

    CalculatorUI

---
+++ index.js
@@ -0,0 +1,9 @@
+function greaterNum(num1, num2) {
+    if (num1 > num2) {
+        return ('theelksf' + (num1));
+    } else {
+        return console.log(num2);
+    }
+}
+
+console.log(greaterNum(10,5));
