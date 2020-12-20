145b3f8852eb53f9ba2afaf25251c6fadfd84c7f
Waiz Khan <68347659+Waiz-Legion@users.noreply.github.com>
2020-12-20 13:59:16 +0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function greaterNum(num1, num2) {
-    if (num1 > num2) {
-        return ('theelksf' + (num1));
-    } else {
-        return console.log(num2);
-    }
-}
-
-console.log(greaterNum(10,5));
